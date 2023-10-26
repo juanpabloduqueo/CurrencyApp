1aab041f7ea6aff32b59a2e5f5ea2c72ec05e9fe
Juan Pablo Duque <juanpabloduqueo@gmail.com>
2023-10-26 14:07:13 -0500

    Initial commit for currency coverter

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+API_KEY = 'DM958STFT5HQABRF'
+
+# This is a template format of the json query
+@app.route('/docs', methods=['GET'])
+def docs():
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo'
+    r = requests.get(url)
+    data = r.json()
+    print(data)
+    return data	
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        # Use try and except to handle errors and avoid app crashing
+        try:
+            # get the request from home.html 
+            amount = request.form['amount']
+            amount = float(amount) # convert to float - display decimals
+            # get the requests from home.html
+            convert_from = request.form['convert_from'] 
+            convert_to = request.form['convert_to']
+            # request data to api with selected currencies
+            url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={convert_from}&to_currency={convert_to}&apikey={API_KEY}'
+            response = requests.get(url).json()
+            # get the exchange rate from the json response
+            Exchange_Rate = response['Realtime Currency Exchange Rate']['5. Exchange Rate']
+            Exchange_Rate = float(Exchange_Rate) # convert to float - display decimals
+            # calculate the conversion
+            result = amount * Exchange_Rate
+            # assign variables to other responses
+            convert_from_code = response['Realtime Currency Exchange Rate']['1. From_Currency Code']
+            convert_from_name = response['Realtime Currency Exchange Rate']['2. From_Currency Name']
+            convert_to_code = response['Realtime Currency Exchange Rate']['3. To_Currency Code']
+            convert_to_name = response['Realtime Currency Exchange Rate']['4. To_Currency Name']
+            updated_time = response['Realtime Currency Exchange Rate']['6. Last Refreshed']
+            return render_template('home.html', result=round(result, 2), amount=amount, 
+                                   convert_from_code=convert_from_code, convert_from_name=convert_from_name, 
+                                   convert_to_code=convert_to_code, convert_to_name=convert_to_name, 
+                                   updated_time=updated_time)
+        # if there is an error, return the home page
+        except Exception as err_msg:
+            return f'<h1> Bad Request : {err_msg} </h1>'
+    else:
+        return render_template('home.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True) 
